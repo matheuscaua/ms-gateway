bfc0f3c51d0e307d4143dc7c98e49f0336fca8d2
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2023-01-20 17:08:14 -0300

    feat - rotas balanceadas

--- src/main/java/com/microservicos/gateway/configuracao/ApiGatewayConfiguracao.java
+++ src/main/java/com/microservicos/gateway/configuracao/ApiGatewayConfiguracao.java
@@ -2,17 +2,20 @@ package com.microservicos.gateway.configuracao;
 
 
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class ApiGatewayConfiguracao {
 
-   /* @Bean
+    @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(p -> p.path("/ms-controle-usuario/**")
-                        .uri("lb:/ms-controle-usuario/"))
+                        .uri("lb://ms-controle-usuario/"))
                 .build();
-    }*/
+    }
 }
