b2b65b0987689718928be9807f1b43467266a62a
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2023-02-22 16:43:05 -0300

    refactor 01

--- src/main/java/com/microservicos/gateway/configuracao/RotasGatewayConfiguracao.java
+++ src/main/java/com/microservicos/gateway/configuracao/RotasGatewayConfiguracao.java
@@ -1,10 +1,12 @@
 package com.microservicos.gateway.configuracao;
 
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class RotasGatewayConfiguracao {
@@ -25,6 +27,12 @@ public class RotasGatewayConfiguracao {
                         .filters(gatewayFilterSpec -> gatewayFilterSpec.setPath("/usuario/{cpf}/{email}"))
                         .uri("lb://ms-controle-usuario"))
                 .build();
+    }
+
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
     }
 
 }
